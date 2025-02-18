a038f2a128eb0fa2ef26df3600cfed21a3700341
olivergoossens <124820590+olivergoossens@users.noreply.github.com>
2025-02-18 16:06:01 +0800

    Update name to Flippy

--- targets/f7/furi_hal/furi_hal_version.c
+++ targets/f7/furi_hal/furi_hal_version.c
@@ -275,7 +275,7 @@ const char* furi_hal_version_get_device_name_ptr(void) {
 }
 
 const char* furi_hal_version_get_ble_local_device_name_ptr(void) {
-    return furi_hal_version.device_name;
+    return "Flippy";
 }
 
 const uint8_t* furi_hal_version_get_ble_mac(void) {
